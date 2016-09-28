365ac455b4ad7677cb75ab6483d7ea3afea8327d
roman25 <ro25man@yandex.ru>
2016-09-28 21:11:50 +0300

    last file

--- testxml.py
+++ testxml.py
@@ -62,7 +62,75 @@ for place in root.findall('Place'):
 						row4[housenumber.tag] = housenumber.text
 						places_data.append(row4)
 
-#2016_09_27/23:38
+for place in root.findall('Place'):		
+	for locationlist in place.findall('LocationList'):
+		for location in locationlist.findall('Location'):
+			for geopositionlist in location.findall('GeoPositionList'):
+				for geoposition in geopositionlist.findall('GeoPosition'):
+					row5 = {}
+					for latitude in geoposition.findall('Latitude'):
+						header.add(latitude.tag)
+						row5[latitude.tag] = latitude.text
+						places_data.append(row5)
+
+					row6 = {}
+					for longitude in geoposition.findall('Longitude'):
+						header.add(longitude.tag)
+						row5[longitude.tag] = longitude.text
+						places_data.append(row6)
+
+					row7 = {}
+					for altitude in geoposition.findall('Altitude'):
+						header.add(altitude.tag)
+						row5[altitude.tag] = altitude.text
+						places_data.append(row7)
+
+
+for place in root.findall('Place'):		
+	for contactlist in place.findall('ContactList'):
+		for contact in contactlist.findall('Contact'):
+			phone = contact.get('type')
+			header.add(phone)
+			row8 = {}
+			for contactstring in contact.findall('ContactString'):
+				row8[phone] = contactstring.text
+				places_data.append(row8)
+
+
+for place in root.findall('Place'):
+	for namelist in place.findall('NameList'):
+		for name in namelist.findall('Name'):
+			for textlist in name.findall('TextList'):
+				for text in textlist.findall('Text'):
+					row9 = {}
+					for basetext in text.findall('BaseText'):
+						official = basetext.get('type')
+						header.add(official)
+						row9[official] = basetext.text
+						places_data.append(row9)
+
+
+for place in root.findall('Place'):
+	for categorylist in place.findall('CategoryList'):
+		for category in categorylist.findall('Category'):
+			findplace = category.get('categorySystem')
+			header.add(findplace)
+			row10 = {}
+			for categoryid in category.findall('CategoryId'):
+				row10[findplace] = categoryid.text
+				places_data.append(row10)
+
+			for categoryname in category.findall('CategoryName'):
+				row11 = {}
+				for textt in categoryname.findall('Text'):
+					header.add(textt.tag)	
+					row11[textt.tag] = textt.text
+					places_data.append(row11)
+
+
+
+
+
 with open('out.csv', 'w') as file:
     writer = csv.DictWriter(file, fieldnames=sorted(header))
     writer.writeheader()
